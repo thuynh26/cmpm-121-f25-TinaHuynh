90bdd8fa9e0b50ffefb6e37b1b16da873d428ec6
thuynh26 <thuynh26@ucsc.edu>
2025-10-03 01:00:40 +0000

    FIxed counter

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@
 console.log("🎮 CMPM 121 - Starting...");
 
 // Simple counter for demonstration
-// deno-lint-ignore prefer-const
+
 let counter: number = 0;
 
 // Create basic HTML structure
@@ -22,5 +22,7 @@ const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
   // This looks like to a good place to add some logic!
+  counter++;
+  counterElement.textContent = counter.toString();
   console.log("I have these thingies:", button, counterElement, counter);
 });
